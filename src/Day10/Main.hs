66e5f2c93e642096f1c700a87f4ade82c403445e
simon <simre4775@gmail.com>
2025-12-10 08:28:29 +0100

    Update

--- src/Day10/Main.hs
+++ src/Day10/Main.hs
@@ -1,35 +1,134 @@
+{-# LANGUAGE TypeAbstractions #-}
+
 module Day10.Main where
 
+import Algorithm.Search (aStar, bfs)
 import Control.Applicative
 import Control.Monad
 import Control.Monad (guard)
+import Control.Monad.ST
+import Data.Bits
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.List qualified as L
 import Data.List.Split qualified as LS
 import Data.Map qualified as M
 import Data.Maybe
+import Data.SBV
+import Data.SBV.Dynamic (CV (..))
+import Data.SBV.Internals (CVal (..))
 import Data.Set qualified as S
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Traversable (for, forM)
 import Data.Vector qualified as V
+import Data.Vector.Unboxed qualified as VU
+import Data.Vector.Unboxed.Mutable qualified as VMU
+import Data.Word
+import Debug.Trace
 import Shared
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as P
 
-type Input = ()
+type Input = [(Word32, V.Vector (VU.Vector Int), VU.Vector Int)]
 
 readInput :: IO Input
-readInput = error "inputs/dayX/input"
+readInput = parseFileMega "inputs/day10/1" $ many $ do
+  i <- indicator
+  b <- buttons
+  j <- joltage
+  P.newline
+  pure $ (i, b, j)
+  where
+    indicator = lexeme $ do
+      P.char '['
+      lights <- many $ P.satisfy (\c -> c == '#' || c == '.')
+      let positions = mapMaybe (\(i, c) -> if c == '#' then Just i else Nothing) $ zip [0 ..] lights
+      P.char ']'
+      pure $ makeBitMask positions
+    buttons = fmap V.fromList $ many $ lexeme $ do
+      P.char '('
+      toggledLights <- lexeme $ P.sepBy numberP (P.char ',')
+      P.char ')'
+      pure (VU.fromList toggledLights)
+    joltage = lexeme $ do
+      P.char '{'
+      joltages <- lexeme $ P.sepBy (fromIntegral <$> numberP) (P.char ',')
+      P.char '}'
+      pure (VU.fromList joltages)
+
+naiveSearch :: V.Vector (VU.Vector Int) -> Word32 -> [Word32]
+naiveSearch buttons goal = fromJust $ bfs nextState (== goal) 0
+  where
+    nextState state = (state `xor`) <$> buttons'
+    buttons' = V.map (makeBitMask . fmap fromIntegral . VU.toList) buttons
+
+naiveJoltageSearch :: V.Vector (VU.Vector Int) -> VU.Vector Int -> (Int, [VU.Vector Int])
+naiveJoltageSearch buttons goal =
+  fromJust $
+    aStar
+      nextState
+      cost
+      estimate
+      isGoal
+      startState
+  where
+    startState = VU.map (const 0) goal
+    isGoal = (== goal)
+    cost _ _ = 1
+    estimate state = VU.maximum $ VU.zipWith (-) goal state
+    nextState :: VU.Vector Int -> V.Vector (VU.Vector Int)
+    nextState state = flip fmap buttons $ \b ->
+      runST $ \ @s -> do
+        mut <- VU.thaw state
+        VU.forM_ b $ \i ->
+          VMU.modify mut (+ 1) i
+        VU.freeze mut
+
+toEquations :: V.Vector (VU.Vector Int) -> VU.Vector Int -> [([Int], Int)]
+toEquations buttons joltages = do
+  zip [0 ..] (VU.toList joltages) <&> \(i, j) ->
+    let usefulButtons = mapMaybe (\(bI, b) -> if VU.elem i b then Just bI else Nothing) (zip [0 ..] (V.toList buttons))
+     in (usefulButtons, j)
+
+makeBitMask :: [Int] -> Word32
+makeBitMask = foldl' setBit 0
 
 solution1 :: Input -> IO ()
-solution1 input = pure ()
+solution1 input =
+  print $ sum $ length . (\(goal, buttons, _) -> naiveSearch buttons goal) <$> input
 
 solution2 :: Input -> IO ()
-solution2 input = pure ()
+solution2 input = do
+  let input' = fmap (\(_, b, j) -> (b, j)) input
+      equations = uncurry toEquations <$> input'
+
+  results <- forM equations $ \equation -> do
+    result <- optimize Lexicographic (model equation)
+    case result of
+      LexicographicResult smt -> case getModelObjectiveValue "total-x-sum" smt of
+        Just mi -> case mi of
+          RegularCV cv -> case cvVal cv of
+            CInteger i -> pure i
+            _ -> error "Not possible"
+          _ -> error "Not possible"
+        _ -> error "Not possible"
+  print $ sum results
+
+-- do
+--   result <- sat model
+--   print result
+model :: [([Int], Int)] -> Symbolic ()
+model equations = do
+  let maxVar = maximum (concatMap fst equations)
+  xs <- V.fromList <$> sIntegers ["x" ++ show i | i <- [0 .. maxVar]]
+
+  let makeConstraint (idxs, s) = sum [xs V.! i | i <- idxs] .== literal (fromIntegral s)
+  mapM_ (constrain . makeConstraint) equations
+  mapM_ (\x -> constrain (x .>= 0)) xs
+  minimize "total-x-sum" (sum xs)
 
 main :: IO ()
 main = do
--- src/Day9/Main.hs
+++ src/Day9/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day9.Main where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad (guard)
+import Data.Bimap qualified as BM
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.List qualified as L
@@ -10,26 +13,124 @@ import Data.List.Split qualified as LS
 import Data.Map qualified as M
 import Data.Maybe
 import Data.Set qualified as S
+import Data.String.Interpolate (__i)
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Traversable (for, forM)
 import Data.Vector qualified as V
+import Debug.Trace
+import Linear.V2
 import Shared
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as P
 
-type Input = ()
+type Input = [V2 Int]
+
+-- 4548962670 -> too high
+-- 4548962670
 
 readInput :: IO Input
-readInput = error "inputs/dayX/input"
+readInput = do
+  file <- readFile "inputs/day9/1"
+  pure $ (\[x, y] -> V2 x y) . fmap read . LS.splitOn "," <$> lines file
+
+rectSizes :: [V2 Int] -> [Int]
+rectSizes ps = [mult (abs (b - a)) | a <- ps, b <- ps, a < b]
+
+mult (V2 x y) = (x + 1) * (y + 1)
+
+makeEdges :: [V2 Int] -> V2 (M.Map Int (S.Set (V2 Int)))
+makeEdges ps = foldl' toLine (pure M.empty) ((last ps, head ps) : zip ps (tail ps))
+  where
+    toLine :: V2 (M.Map Int (S.Set (V2 Int))) -> (V2 Int, V2 Int) -> V2 (M.Map Int (S.Set (V2 Int)))
+    toLine (V2 vertical horizontal) (V2 x1 y1, V2 x2 y2)
+      | x1 == x2 =
+          let yLine = if y1 > y2 then V2 y2 y1 else V2 y1 y2
+           in V2 (M.alter (Just . maybe (S.singleton yLine) (S.insert yLine)) x1 vertical) horizontal
+      | y1 == y2 =
+          let xLine = if x1 > x2 then V2 x2 x1 else V2 x1 x2
+           in V2 vertical (M.alter (Just . maybe (S.singleton xLine) (S.insert xLine)) y1 horizontal)
+      | otherwise = error "No lines"
+
+zipWithV2 :: (a -> b -> c) -> V2 a -> V2 b -> V2 c
+zipWithV2 f (V2 x1 y1) (V2 x2 y2) = V2 (f x1 x2) (f y1 y2)
+
+getLines :: V2 (M.Map Int (S.Set (V2 Int))) -> V2 Int -> V2 ([V2 Int], Maybe (V2 Int), [V2 Int])
+getLines (V2 vertical horizontal) (V2 x y) =
+  let (left, middle, right) = M.splitLookup x vertical
+      (bottom, center, top) = M.splitLookup y horizontal
+      f i c ls = catMaybes $ M.elems $ withinRange i <$> ls
+   in V2
+        (f y x left, middle >>= withinRange y, f y x right)
+        (f x y bottom, center >>= withinRange x, f x y top)
+
+intersect :: V2 (M.Map Int (S.Set (V2 Int))) -> V2 Int -> V2 Int -> V2 Int -> Bool
+intersect (V2 vertical horizontal) (V2 cx cy) (V2 x1 y1) (V2 x2 y2)
+  | x1 == x2 = intersect' horizontal cx (reorder $ V2 y1 y2) x1
+  | y1 == y2 = intersect' vertical cy (reorder $ V2 x1 x2) y2
+  where
+    reorder (V2 a b) = if a < b then V2 a b else V2 b a
+    makeSmaller (V2 a b) = (V2 (a + 1) (b - 1))
+    intersect' :: M.Map Int (S.Set (V2 Int)) -> Int -> V2 Int -> Int -> Bool
+    intersect' lines center (V2 a b) i =
+      let (_, lines') = M.split a lines
+          (lines'', _) = M.split b lines'
+          ls = mconcat $ M.elems lines''
+          intersections = S.filter (\(V2 a b) -> a <= i && i <= b) ls
+          skewed = i + signum (center - i)
+       in -- in traceShow (intersections, skewed, center) $ S.null $ S.filter (\(V2 a b) -> a <= skewed && skewed <= b) intersections
+          S.null $ S.filter (\(V2 a b) -> a <= skewed && skewed <= b) intersections
+
+withinRange i set = case S.lookupLT (V2 i maxBound) set of
+  Nothing -> Nothing
+  Just (V2 start end) -> if i <= end then Just (V2 start end) else Nothing
+
+toSVG :: [V2 Int] -> String
+toSVG (V2 x y : ps) =
+  [__i|
+    <svg
+     width="100000"
+     height="100000"
+    >
+    <path
+      style="fill:\#000000;stroke-width:1;stroke:\#000000;stroke-opacity:1"
+      d="M #{x} #{y} #{toPaths ps} Z"
+    />
+    </svg>
+  |]
+  where
+    toPaths :: [V2 Int] -> String
+    toPaths (V2 x y : ps) =
+      [__i|
+        L #{x} #{y}
+    |]
+        ++ toPaths ps
+    toPaths _ = ""
+
+rects :: [V2 Int] -> [(V2 Int, V2 Int)]
+rects ps = [(a, b) | a <- ps, b <- ps, a < b]
 
 solution1 :: Input -> IO ()
-solution1 input = pure ()
+solution1 input =
+  print $ maximum $ rectSizes input
+
+check :: V2 (M.Map Int (S.Set (V2 Int))) -> (V2 Int, V2 Int) -> Bool
+check edges (V2 x1 y1, V2 x2 y2) =
+  let rectLines = [(V2 x1 y1, V2 x2 y1), (V2 x2 y1, V2 x2 y2), (V2 x2 y2, V2 x1 y2), (V2 x1 y2, V2 x1 y1)]
+      rectCenter = V2 ((x2 + x1) `quot` 2) ((y2 + y1) `quot` 2)
+   in all (uncurry (intersect edges rectCenter)) rectLines
+
+pointCheck :: ([V2 Int], Maybe (V2 Int), [V2 Int]) -> V2 Int
+pointCheck (l, m, r) = case m of
+  Just _ -> if even (length l) then V2 (length l + 1) (length r) else V2 (length l) (length r + 1)
+  Nothing -> V2 (length l) (length r)
 
 solution2 :: Input -> IO ()
-solution2 input = pure ()
+solution2 input = do
+  let edges = makeEdges input
+  print $ maximum $ (\(a, b) -> (mult . fmap abs $ b - a, a, b)) <$> filter (check edges) (rects input)
 
 main :: IO ()
 main = do
